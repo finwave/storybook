d7b9e9d2bccb1d5f4e763b7232d7ee97db604147
Leo Laine <leo.laine.1980@gmail.com>
2026-03-06 14:28:18 +0200

    localization (en, fi) / resource files
    localized every text so far

--- StoryBook/Program.cs
+++ StoryBook/Program.cs
@@ -2,12 +2,22 @@ using StoryBook.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add localization services
+builder.Services.AddLocalization();
 // Add services to the container.
 builder.Services.AddRazorComponents().AddInteractiveServerComponents();
 // BootstrapBlazor
 builder.Services.AddBootstrapBlazor();
 
 var app = builder.Build();
+
+string[] supportedCultures = ["en-US", "fi-FI"];
+var localizationOptions = new RequestLocalizationOptions()
+    .SetDefaultCulture(supportedCultures[0])
+    .AddSupportedCultures(supportedCultures)
+    .AddSupportedUICultures(supportedCultures);
+
+app.UseRequestLocalization(localizationOptions);
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
